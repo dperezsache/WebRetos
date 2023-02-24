8faaa03be26cc69920dd2b6bde3642af4e52f9de
David Pérez <dperezsache.guadalupe@alumnado.fundacionloyola.net>
2023-02-24 12:50:44 +0100

    Fixes para subida a hosting

--- php/controller/controladorcategorias.php
+++ php/controller/controladorcategorias.php
@@ -1,5 +1,5 @@
 <?php
-    require_once(dirname(__DIR__) .'../model/modelocategorias.php');
+    require_once(dirname(__DIR__) . '/model/modelocategorias.php');
 
     /**
      * Clase ControladorCategorias.
